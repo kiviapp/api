c9827ef3939e5bf66e0f4e1e9e311f138b91572d
Barış DEMİRCİ <demirci.baris38@gmail.com>
2021-01-10 18:01:50 +0000

    🎨 fastify initialization

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { ValidationPipe } from "@nestjs/common";
+import * as helmet from "fastify-helmet";
+import * as multer from "fastify-multer";
+import {
+	FastifyAdapter,
+	NestFastifyApplication,
+} from "@nestjs/platform-fastify";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create<NestFastifyApplication>(
+		AppModule,
+		new FastifyAdapter(),
+	);
+	app.getHttpAdapter()
+		.getInstance()
+		.register(multer.contentParser)
+		.register(helmet);
+	app.enableCors();
+	app.useGlobalPipes(
+		new ValidationPipe({
+			transform: true,
+		}),
+	);
+	await app.listen(process.env.PORT || 3000, "0.0.0.0");
 }
 bootstrap();
